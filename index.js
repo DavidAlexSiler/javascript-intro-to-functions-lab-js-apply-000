f9f578a31ed447372e3bcf4190c545ecd161ebbb
Alex Siler <davidalexsiler@gmail.com>
2019-04-03 21:34:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if(string.toLowerCase() === string){
     return "I can't hear you!";
+    }
+    else if(string === "I love you, Grandma."){
+      return "I love you, too.";
   }else{
     return "YES INDEED!";
   }
